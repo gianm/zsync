72e5b58d33d277d9f220c32e20e28caaf3b62a79
merlino <merlino@7e849ce6-0c56-0410-a18e-85f50c794e95>
2011-01-05 21:51:02 +0000

    zsync.h: add zsync_filelen, since it's used by client.c

--- c/libzsync/zsync.h
+++ c/libzsync/zsync.h
@@ -25,6 +25,8 @@ int zsync_hint_decompress(const struct zsync_state*);
 
 /* zsync_filename - return the suggested filename from the .zsync file */
 char* zsync_filename(const struct zsync_state*);
+/* zsync_filelen - return the length from the .zsync file */
+off_t zsync_filelen(const struct zsync_state*);
 /* zsync_mtime - return the suggested mtime from the .zsync file */
 time_t zsync_mtime(const struct zsync_state*);
 
